d1fead5b37ff3df415cbb85d214c8c0119b2db1c
vishalkumar-sahu <vishalkumar.sahu@gmail.com>
2023-02-05 22:58:34 +0530

    backend dev

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+const cors = require("cors");
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'});
+
+const mongoose = require('mongoose');
+
+async function main(){
+    try{
+        mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 5000})
+        
+        mongoose.connection.on('connected', ()=>{
+            console.log('Connected MongoDb Atlas..');
+        });
+        
+    
+    }catch(error){
+        console.log(error.reason)
+    }
+  
+}
+main();
+
+require('./models/user');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use(require('./routes/auth'));
+// app.use(require('./routes/post'));
+
+if( process.env.NODE_ENV === "production" ){
+    app.use(express.static("client/build"));
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Connected on port : ${PORT}`);
+});
